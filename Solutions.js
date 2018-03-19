78fcacdad42091452be09b581daf6004fc025917
nicktayl0r <nicholasjt22@gmail.com>
2018-03-18 23:13:22 -0700

    Update Solutions.js

--- Solutions.js
+++ Solutions.js
@@ -47,3 +47,56 @@ function diamond(n){
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//03.18.18
+//https://www.codewars.com/kata/57814d79a56c88e3e0000786
+//Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
+
+
+
+function encrypt(text, n) {
+  if (!text || !n || n<=0) { return text };
+  const reducer = (accumulator, letter) => accumulator + letter;
+  if (text) { text = text.split('')} else {return text};
+  let i = -1;
+  while((i+=1) < n ){
+    let newText = "";
+    let j = -1;
+    while((j+=2) < text.length){
+      newText += text.splice(j, 1, '');
+    }
+    text = text.reduce(reducer);
+    newText += text;
+    text = newText.split('');
+  }
+  return text.join('');
+}
+
+
+
+function decrypt(encryptedText, n) {
+  if (!encryptedText || !n || n<=0) { return encryptedText }
+  
+  const decryptr = (cipherText) => {
+    let odds = cipherText.slice(0, (cipherText.length/2));
+    let evens = cipherText.slice((cipherText.length/2));
+    let out = '';
+    
+    for (let i = 0; i < cipherText.length; i++){
+      if (i % 2){
+        out += odds[0];
+        odds = odds.slice(1);
+      } else {
+        out += evens[0];
+        evens = evens.slice(1);
+      }
+    }
+    return out;
+  }
+  
+  for(let i = 0;i<n;i++) {
+    encryptedText = decryptr(encryptedText);
+  }
+  
+  return encryptedText;
+}
