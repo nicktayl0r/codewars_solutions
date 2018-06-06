a5a30ac01b8a89e16ff4cb4246c966924d2cc628
nicktayl0r <nicholasjt22@gmail.com>
2018-06-06 16:33:38 -0700

    duplicateCount

--- Solutions.js
+++ Solutions.js
@@ -281,4 +281,22 @@ const kebabize = (camel) => {
   return output;
 }
 
+//06.06.18
+//https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/
+//Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+
+const duplicateCount = (text) => {
+  text = text.toLowerCase();
+  let state = {letters:[], d:0};
+  for(i = 0; i < text.length; i++) {
+    let isDuplicated = text.substring(i+1).includes(text[i]);
+    let isRecorded = state.letters.includes(text[i]);
+    if(isDuplicated && !isRecorded){
+      state.d++;
+      state.letters.push(text[i])
+    }
+  }
+  return state.d;
+}
 
