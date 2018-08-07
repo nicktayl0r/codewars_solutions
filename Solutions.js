debef76a4a7cdbeca7f9f3f33ae3a0a89daf9593
nicktayl0r <nicholasjt22@gmail.com>
2018-08-07 13:30:38 -0700

    Update Solutions.js

--- Solutions.js
+++ Solutions.js
@@ -323,3 +323,13 @@ const balance = (left,right) => {
    return "Balance";
   }
 }
+
+//08.07.18
+//https://www.codewars.com/kata/583203e6eb35d7980400002a/
+//return the total number of smiling faces in the array
+const countSmileys = (arr) => {
+  let array = arr.filter((a) => 
+    a.match(/^[:;]([-~][D)]|[D)])/g)
+  )
+  return array.length
+}
